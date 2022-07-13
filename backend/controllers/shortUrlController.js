4e371200667bfde032a339f09ee0387b1e3d7e64
Ariwan Sri Setya <77198285+ariwanss@users.noreply.github.com>
2022-07-13 05:24:09 +0000

    remove conditional in get request controller where if url does not start with http, add 'http' to the beginning of the url

--- backend/controllers/shortUrlController.js
+++ backend/controllers/shortUrlController.js
@@ -3,10 +3,12 @@ const ShortUrl = require('../models/shortUrlModel');
 const { updateCounter } = require('../config/counter');
 
 const createUrl = asyncHandler(async (req, res) => {
+  console.log('create: ' + req.body.url);
   let shortUrl = await ShortUrl.create({
     originalUrl: req.body.url,
     shortUrl: (await updateCounter('urlCounter')).lastValue
   })
+  console.log('create: ' + shortUrl.shortUrl);
   res.status(200).json({
     original_url: shortUrl.originalUrl,
     short_url: shortUrl.shortUrl
@@ -14,13 +16,11 @@ const createUrl = asyncHandler(async (req, res) => {
 });
 
 const getUrl = asyncHandler(async (req, res) => {
+  console.log('read: ' + req.params.id);
   let shortUrl = await ShortUrl.findOne({ shortUrl: req.params.id });
-  let protocolRegex = /^http/;
-  if (protocolRegex.test(shortUrl.originalUrl)) {
-    res.redirect(shortUrl.originalUrl);
-  } else {
-    res.redirect('https://' + shortUrl.originalUrl);
-  }
+  console.log('read: ' + shortUrl.shortUrl);
+  console.log('read: ' + shortUrl.originalUrl);
+  res.redirect(shortUrl.originalUrl);
 });
 
 module.exports = {
