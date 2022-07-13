601531550e81b516529d2e85e387944fe0cdc593
ariwanss <ariwansrisetya@gmail.com>
2022-07-13 12:02:16 +0700

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const path = require('node:path');
 const connectDB = require('./config/db');
 const {errorHandler} = require('./middleware/errorMiddleware');
@@ -10,7 +11,7 @@ const port = 3000;
 const app = express();
 connectDB();
 initCounter('urlCounter');
-
+app.use(cors({optionsSuccessStatus: 200}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
